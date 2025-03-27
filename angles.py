543d5b41538564c0bd2b2ed26dac9f96a882d2ed
xrhuang10 <xrhuang10@gmail.com>
2025-03-27 17:53:31 -0400

    renamed variables

--- angles.py
+++ angles.py
@@ -5,12 +5,13 @@ l2 = 180
 l3 = 73.29
 x = 212.04
 y = 37.82
+dmotor = 94.13
 
 i = 166
 j = 83.55
 
 def thetaprime(x, y):
-    numerator = x * x + pow(94.13 - y, 2) - l1 * l1 - l2 * l2
+    numerator = x * x + pow(dmotor - y, 2) - l1 * l1 - l2 * l2
     denominator = -2 * l1 * l2
     thetaprime = np.degrees(np.arccos(numerator/denominator))
     return thetaprime
@@ -18,7 +19,7 @@ def thetaprime(x, y):
 
 def theta(x, y):
     theta2 = np.degrees(np.arcsin((l2*(np.sin(np.radians(thetaprime(x, y))))) / (np.sqrt(l1*l1 + l2*l2 - 2*l1*l2*(np.cos(np.radians(thetaprime(x, y))))))))
-    theta3 = np.degrees(np.arctan(x / (94.13 - y)))
+    theta3 = np.degrees(np.arctan(x / (dmotor - y)))
     # print(thetaprime(x, y))
     # print(theta2)
     # print(theta3)
@@ -33,7 +34,7 @@ def phi(x, y):
 
     d = l1*np.cos(np.radians(theta(x, y)))
     g = l3*np.sin(np.radians(90 + theta(x, y) - thetaprime(x, y)))
-    y2 = 94.13 + d - g
+    y2 = dmotor + d - g
     def phi1():
         answer = np.degrees(np.arctan(x2 / y2))
         return answer
