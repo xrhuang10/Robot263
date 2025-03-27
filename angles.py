d4a6c7cec75ca4a35c24dae7cbd899b1569dfa36
xrhuang10 <xrhuang10@gmail.com>
2025-03-27 18:18:46 -0400

    renamed and described variables

--- angles.py
+++ angles.py
@@ -1,14 +1,22 @@
+#X is the shortest distance from the gripper to metal bar
+#Y is the distance from the gripper to the phi motor parallel to the metal bar
+
+#l1 is the length of the bar attached to the theta motor
+#l2 is the length of the bar connecting l1 tip to the gripper
+#l3 is the length of l2 but cut off where it meets l4
+#l4 is the length of the bar connecting l5 tip and l3 tip (intersecting l2)
+#l5 is the length of the bar attached to the phi motor
+#dmotor is the separation between the theta and phi motors
+
 import numpy as np
 
 l1 = 83.55
 l2 = 180
 l3 = 73.29
-x = 212.04
-y = 37.82
-dmotor = 94.13
+l4 = 166
+l5 = 83.55
 
-i = 166
-j = 83.55
+dmotor = 94.13
 
 def thetaprime(x, y):
     numerator = x * x + pow(dmotor - y, 2) - l1 * l1 - l2 * l2
@@ -40,8 +48,9 @@ def phi(x, y):
         return answer
     
     def phi2():
+        #h is the distance between the phi motor point and the gripper
         h = np.sqrt(x2*x2 + y2*y2)
-        answer = np.degrees(np.arccos((i*i - h*h - j*j) / (-2*h*j)))
+        answer = np.degrees(np.arccos((l4*l4 - h*h - l5*l5) / (-2*h*l5)))
         return answer
     
     return phi1() + phi2()
